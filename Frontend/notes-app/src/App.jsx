a61fc662e7185132565e6b5876f156ea6eb210f7
archana212 <archukanzariya17@gmail.com>
2025-01-16 07:50:25 +0530

    started notes app and did login screen

---
+++ Frontend/notes-app/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
+import Home from "./pages/Home/home";
+import Login from "./pages/Login/login";
+import SignUp from "./pages/SignUp/signup";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/Login" exact element={<Login />} />
+      <Route path="/SignUp" exact element={<SignUp />} />
+    </Routes>
+  </Router>
+);
+  
+
+const App = () => {
+    return <div>{routes}</div>;
+};
+
+export default App
