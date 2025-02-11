3ce2a8daa7e3ad8a6f31f9fbd76c0001ab774982
archana212 <archukanzariya17@gmail.com>
2025-02-11 22:49:14 +0530

    Updated

--- Frontend/notes-app/src/pages/Home/home.jsx
+++ Frontend/notes-app/src/pages/Home/home.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
 
 const Home = () => {
+
+    const [openAddEditModal, setOpenAddEditModel] = useState({
+        isShown: false,
+        type:"add",
+        data:null,
+    });
     return(
        <>
         <Navbar />
@@ -57,13 +64,29 @@ const Home = () => {
             </div>
         </div>
 
-        <button className="w-17 h-16 flex items-center justify-center rounded-2xl bg-blue-600 absolute right-10 bottom-10" onClick={() => {}}>
+        <button className="w-16 h-16 flex items-center justify-center rounded-2xl bg-blue-600 absolute right-10 bottom-10"
+         onClick={() => {
+            setOpenAddEditModel({isShown: true, type:"add",data:null});
+         }}>
             <MdAdd className="text-[32px] text-white"/>
         </button>
 
-        <AddEditNotes />
-       </>
-    )
-}
 
-export default Home
+        <Modal
+        isOpen={openAddEditModal.isShown}
+        onRequestClose={() => {}}
+        style={{
+            overlay: {
+                backgroundColor: "rgba(0,0,0,0.2)",
+            },
+        }}
+        contentLabel=""
+        className="w-[60%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+        >
+        <AddEditNotes />
+        </Modal>
+       </>
+    );
+};
+
+export default Home;
